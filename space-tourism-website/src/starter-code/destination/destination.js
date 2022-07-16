dea28d086812c1726d633ced351a61b8e8fbbd0c
ThibWill <thibault.willer@gmail.com>
2022-07-16 22:41:34 +0200

    Full destination

--- space-tourism-website/src/starter-code/destination/destination.js
+++ space-tourism-website/src/starter-code/destination/destination.js
@@ -1,4 +1,47 @@
 window.onload = () => {
+  /*
+  * HEADER
+  */
+  const MOBILE_TRESHHOLD = 481;
+    
+  const burger = document.querySelector("#burger");
+  const close = document.querySelector("#close");
+  const navBar = document.querySelector("#navBar");
+
+
+  function displayMenu() {
+      burger.style['display'] = 'none';
+      navBar.style['display'] = 'flex';
+  }
+
+  function removeMenu() {
+      burger.style['display'] = 'block';
+      navBar.style['display'] = 'none';
+  }
+
+  burger.addEventListener('click', () => {
+      displayMenu();
+  });
+
+  close.addEventListener('click', () => {
+      removeMenu();
+  });
+
+  (function(){
+      let previousState = window.innerWidth;
+      window.addEventListener('resize', () => {
+          if (window.innerWidth > MOBILE_TRESHHOLD) {
+              displayMenu();
+          } else if (previousState && previousState > MOBILE_TRESHHOLD) {
+              removeMenu();
+          }
+          previousState = window.innerWidth;
+      });
+  }());
+
+  /*
+  * DESTINATION
+  */
   const moonDom = document.querySelector('#moon');
   const marsDom = document.querySelector('#mars');
   const europaDom = document.querySelector('#europa');
