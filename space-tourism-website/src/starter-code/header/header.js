7d1dc4776041099ae4d53ff0cd688e6ed6d4a9fc
ThibWill <thibault.willer@gmail.com>
2022-07-18 23:12:46 +0200

    :sparkles: Crew start

--- space-tourism-website/src/starter-code/header/header.js
+++ space-tourism-website/src/starter-code/header/header.js
@@ -1,38 +1,36 @@
-window.onload = () => {
-    const MOBILE_TRESHHOLD = 481;
-    
-    const burger = document.querySelector("#burger");
-    const close = document.querySelector("#close");
-    const navBar = document.querySelector("#navBar");
+const MOBILE_TRESHHOLD = 481;
+
+const burger = document.querySelector("#burger");
+const close = document.querySelector("#close");
+const navBar = document.querySelector("#navBar");
 
 
-    function displayMenu() {
-        burger.style['display'] = 'none';
-        navBar.style['display'] = 'flex';
-    }
-
-    function removeMenu() {
-        burger.style['display'] = 'block';
-        navBar.style['display'] = 'none';
-    }
-
-    burger.addEventListener('click', () => {
-        displayMenu();
-    });
-
-    close.addEventListener('click', () => {
-        removeMenu();
-    });
-
-    (function(){
-        let previousState = window.innerWidth;
-        window.addEventListener('resize', () => {
-            if (window.innerWidth > MOBILE_TRESHHOLD) {
-                displayMenu();
-            } else if (previousState && previousState > MOBILE_TRESHHOLD) {
-                removeMenu();
-            }
-            previousState = window.innerWidth;
-        });
-    }());
+function displayMenu() {
+    burger.style['display'] = 'none';
+    navBar.style['display'] = 'flex';
 }
+
+function removeMenu() {
+    burger.style['display'] = 'block';
+    navBar.style['display'] = 'none';
+}
+
+burger.addEventListener('click', () => {
+    displayMenu();
+});
+
+close.addEventListener('click', () => {
+    removeMenu();
+});
+
+(function(){
+    let previousState = window.innerWidth;
+    window.addEventListener('resize', () => {
+        if (window.innerWidth > MOBILE_TRESHHOLD) {
+            displayMenu();
+        } else if (previousState && previousState > MOBILE_TRESHHOLD) {
+            removeMenu();
+        }
+        previousState = window.innerWidth;
+    });
+}());
